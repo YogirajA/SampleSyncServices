6541135a7bbd68fd9cb2527f8b5b686e463d141d
yogirajA <yogi.aradhye@hotmail.com>
2017-08-31 13:58:38 -0500

    created folder for two endpoints

---
+++ src/Two Endpoints/SyncUsersEndpoint/SqlServerEndpoint.cs
@@ -0,0 +1,23 @@
+namespace SyncUsersEndpoints
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+    public class SqlServerEndpoint
+    {
+        public static IEndpointInstance Instance { get; private set; }
+        public  static async Task StartInstance()
+        {
+            var endpointConfiguration = new EndpointConfiguration("SyncUsers.SqlServerEndpoint");
+            var transport = endpointConfiguration.UseTransport<SqlServerTransport>()
+                .ConnectionStringName("UsersAppDatabase");
+            transport.Transactions(TransportTransactionMode.None);
+
+            endpointConfiguration.UsePersistence<InMemoryPersistence>();
+            endpointConfiguration.SendFailedMessagesTo("error");
+            endpointConfiguration.AuditProcessedMessagesTo("audit");
+            endpointConfiguration.UseSerialization<JsonSerializer>();
+            Instance = await Endpoint.Start(endpointConfiguration);
+        }
+        
+    }
+}
