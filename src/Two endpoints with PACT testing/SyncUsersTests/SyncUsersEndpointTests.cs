8add6f416452aea58d54805881ea0551a47a033b
yogirajA <yogi.aradhye@hotmail.com>
2017-10-31 13:19:41 -0500

    Upgraded projects to 4.7.1

--- src/Two endpoints with PACT testing/SyncUsersTests/SyncUsersEndpointTests.cs
+++ src/Two endpoints with PACT testing/SyncUsersTests/SyncUsersEndpointTests.cs
@@ -26,33 +26,6 @@
             Should.NotThrow(async () => await handler.Handle(new NewUser(), context).ConfigureAwait(false));
             
         }
-		//https://docs.particular.net/nservicebus/messaging/evolving-contracts
-		[Fact]
-        public void TestServicePact()
-        {
-	        var ob = typeof(NewUser);
-
-			var properties = ob.GetProperties();
-
-	        properties.Length.ShouldBe(5);
-
-	        AssertPropertySpecs(properties.FirstOrDefault(t => t.Name.Equals(nameof(NewUser.Id))), typeof(Guid));
-
-			AssertPropertySpecs(properties.FirstOrDefault(t => t.Name.Equals(nameof(NewUser.FirstName))), typeof(string));
-	        
-			AssertPropertySpecs(properties.FirstOrDefault(t => t.Name.Equals(nameof(NewUser.LastName))), typeof(string));
-
-            AssertPropertySpecs(properties.FirstOrDefault(t => t.Name.Equals(nameof(NewUser.CreatedOn))), typeof(DateTime));
-	        AssertPropertySpecs(properties.FirstOrDefault(t => t.Name.Equals(nameof(NewUser.ModifiedOn))), typeof(DateTime));
-        }
-
-	    private static void AssertPropertySpecs(PropertyInfo firstOrDefault, Type propertyType)
-	    {
-		    firstOrDefault.ShouldNotBeNull();
-
-		    firstOrDefault.PropertyType.ShouldBe(propertyType);
-
-		    firstOrDefault.PropertyType.IsPublic.ShouldBeTrue();
-	    }
+		   
     }
 }
